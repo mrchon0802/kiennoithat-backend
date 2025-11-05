a15e09716c2d54957eb3511df9062541047c75c6
mrchon0802 <callmerio69@gmail.com>
2025-11-05 15:34:46 +0700

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     console.error('❌ MongoDB connection error:', err);
   });
 
-  await app.listen(process.env.PORT || 5000);
-  console.log(`✅ Backend running on http://localhost:5000`);
+  await app.listen(process.env.PORT || 5000, '0.0.0.0');
+  console.log(`✅ Backend running on port ${process.env.PORT || 5000}`);
 }
 bootstrap();
